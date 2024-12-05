1b81a341fdca0be6d87a1fad316a3e6402bffe78
Tiffany Timbers <tiffany.timbers@gmail.com>
2024-12-05 10:54:10 -0800

    some more edge cases

--- tests/test_validate_data.py
+++ tests/test_validate_data.py
@@ -49,6 +49,12 @@ def test_valid_data_type():
     with pytest.raises(TypeError):
         validate_data(valid_data_as_np)
 
+# Case: empty data frame
+case_empty_data_frame = valid_data.copy().iloc[0:0]
+def test_valid_data_empty_data_frame():
+    with pytest.raises(ValueError):
+        validate_data(case_empty_data_frame)
+
 # Setup list of invalid data cases 
 invalid_data_cases = []
 
@@ -61,22 +67,34 @@ invalid_data_cases.append((case_missing_class, "Check absent or incorrect for mi
 case_wrong_label_type = valid_data.copy()
 case_wrong_label_type["class"] = case_wrong_label_type["class"].map({'Benign': 0, 'Malignant': 1})
 invalid_data_cases.append((case_missing_class, "Check incorrect type for'class' values is missing or incorrect"))
+
+# Case: missing "class" column
+case_missing_class_col = valid_data.copy()
+case_missing_class_col = case_missing_class_col.drop("class", axis=1)  # drop class column
+invalid_data_cases.append((case_missing_class_col, "`class` from DataFrameSchema"))
+
+# Case: missing numeric columns (one for each numeric column) where column is missing
+numeric_columns = valid_data.select_dtypes(include=np.number).columns
+for col in numeric_columns:
+    case_missing_col = valid_data.copy()
+    case_missing_col = case_missing_col.drop(col, axis=1)  # drop column
+    invalid_data_cases.append((case_missing_col, f"'{col}' is missing from DataFrameSchema"))
     
-# Generate 30 cases Case (one for each numeric column) where data is out of range (too large)
+# Generate 30 cases (one for each numeric column) where data is out of range (too large)
 numeric_columns = valid_data.select_dtypes(include=np.number).columns
 for col in numeric_columns:
     case_too_big = valid_data.copy()
     case_too_big[col] = case_too_big[col] + 10  # Adding an arbitrary value to make it out of range
     invalid_data_cases.append((case_too_big, f"Check absent or incorrect for numeric values in '{col}' being too large"))
 
-# Generate 30 cases Case (one for each numeric column) where data is out of range (too small)
+# Generate 30 cases (one for each numeric column) where data is out of range (too small)
 numeric_columns = valid_data.select_dtypes(include=np.number).columns
 for col in numeric_columns:
     case_too_small = valid_data.copy()
     case_too_small[col] = case_too_small[col] - 10  # Adding an arbitrary value to make it out of range
     invalid_data_cases.append((case_too_small, f"Check absent or incorrect for numeric values in '{col}' being too small"))
 
-# Generate 30 cases Case (one for each numeric column) where data is the wrong type
+# Generate 30 cases (one for each numeric column) where data is the wrong type
 numeric_columns = valid_data.select_dtypes(include=np.number).columns
 for col in numeric_columns:
     case_wrong_type = valid_data.copy()
@@ -96,6 +114,6 @@ invalid_data_cases.append((case_missing_obs, f"Check absent or incorrect for mis
 
 # Parameterize invalid data test cases
 @pytest.mark.parametrize("invalid_data, description", invalid_data_cases)
-def test_valid_data(invalid_data, description):
+def test_valid_w_invalid_data(invalid_data, description):
     with pytest.raises(pa.errors.SchemaErrors):
         validate_data(invalid_data)
